f12a8a2330d748378a88094ae9b99955678f14b7
SorokaVlad17 <44582306+SorokaVlad17@users.noreply.github.com>
2019-08-24 21:20:59 +0300

    Create Compare.cs

---
+++ Compare.cs
@@ -0,0 +1,70 @@
+using System;
+
+namespace Project
+{
+    class Program
+    {
+        public  static int Compare(string a, string b)
+        {
+            if (a == b)
+            {
+                return 0;
+            }
+            else
+            {
+
+                if (a.Length == b.Length)
+                {
+
+                    int sumaFirstStr = 0, sumaSecondStr = 0;
+
+                    foreach (char i in a.ToCharArray())
+                    {
+                        sumaFirstStr += (int)i;
+                    }
+
+                    foreach (char j in b.ToCharArray())
+                    {
+                        sumaSecondStr += (int)j;
+                    }
+
+                    if (sumaFirstStr < sumaSecondStr)
+                    {
+                        return -1;
+                    }
+
+                    else
+                    {
+                        return 1;
+                    }
+                }
+
+                else
+                {
+                    if(a.Length<b.Length)
+                    {
+                        return -1;
+                    }
+
+                    else
+                    {
+                        return 1;
+                    }
+                }
+
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первую строку : ");
+            string str1 = Console.ReadLine();
+
+            Console.Write("Введите вторую строку : ");
+            string str2 = Console.ReadLine();
+
+            Console.WriteLine("String.Compare(\"{0}\",\"{1}\") = {2}",str1,str2,string.Compare(str1,str2));
+
+            Console.WriteLine("Program.Compare(\"{0}\",\"{1}\") = {2}", str1, str2, Compare(str1, str2));
+        }
+    }
+}
