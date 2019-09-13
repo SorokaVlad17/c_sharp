2c77b6c7438b01bb3cfc8affe29a7c3bd324f678
SorokaVlad17 <44582306+SorokaVlad17@users.noreply.github.com>
2019-09-13 16:21:21 +0300

    Add files via upload

---
+++ swap_function/swap_function/Program.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace swap_function
+{
+    class Program
+    {
+        public static void swap(ref int a, ref int b)
+        {
+            int boof = a;
+            a = b;
+            b = boof;
+        }
+        public static void swap(ref byte a, ref byte b)
+        {
+            byte boof = a;
+            a = b;
+            b = boof;
+        }
+        public static void swap(ref double a, ref double b)
+        {
+            double boof = a;
+            a = b;
+            b = boof;
+        }
+        public static void swap(ref float a, ref float b)
+        {
+            float boof = a;
+            a = b;
+            b = boof;
+        }
+        public static void swap(ref short a, ref short b)
+        {
+            short boof = a;
+            a = b;
+            b = boof;
+        }
+        public static void swap(ref ushort a, ref ushort b)
+        {
+            ushort boof = a;
+            a = b;
+            b = boof;
+        }
+        public static void swap(ref sbyte a, ref sbyte b)
+        {
+            sbyte boof = a;
+            a = b;
+            b = boof;
+        }
+        public static void swap(ref uint a, ref uint b)
+        {
+            uint boof = a;
+            a = b;
+            b = boof;
+        }
+        public static void swap(ref long a, ref long b)
+        {
+            long boof = a;
+            a = b;
+            b = boof;
+        }
+        public static void swap(ref ulong a, ref ulong b)
+        {
+            ulong boof = a;
+            a = b;
+            b = boof;
+        }
+
+        public static void swap(ref decimal a, ref decimal b)
+        {
+            decimal boof = a;
+            a = b;
+            b = boof;
+        }
+        public static void swap(ref bool a, ref bool b)
+        {
+            bool boof = a;
+            a = b;
+            b = boof;
+        }
+
+        public static void swap(ref object a, ref object b)
+        {
+            object boof = a;
+            a = b;
+            b = boof;
+        }
+
+        public static void swap(ref string a, ref string b)
+        {
+            string boof = a;
+            a = b;
+            b = boof;
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
