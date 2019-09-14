49d0662b6d36ee43fa474a0d66f3adb13ae9e97d
SorokaVlad17 <44582306+SorokaVlad17@users.noreply.github.com>
2019-09-14 12:59:25 +0300

    Update Program.cs

--- MovingMouse/MovingMouse/Program.cs
+++ MovingMouse/MovingMouse/Program.cs
@@ -35,7 +35,7 @@ namespace MovingMouse
                 }
                 catch
                 {
-                    continue;
+                   
                 }
 
                 xMonitorBefore = xMonitorNow;
