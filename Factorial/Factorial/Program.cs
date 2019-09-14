d47dae2509a03ca242617f70dc083e01365ea3c3
SorokaVlad17 <44582306+SorokaVlad17@users.noreply.github.com>
2019-09-14 14:49:35 +0300

    Add files via upload

---
+++ Factorial/Factorial/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorial
+{
+    class Program
+    {
+        public static int Factorial(int number)
+        {
+            
+            if(number==0)
+            {
+                return 1;
+            }
+            return number * Factorial(number - 1);
+        }
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
