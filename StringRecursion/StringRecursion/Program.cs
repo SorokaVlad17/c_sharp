2d964dbb393bb1ba8f280e2bd2eab964dbbe9dfb
SorokaVlad17 <44582306+SorokaVlad17@users.noreply.github.com>
2019-09-14 21:34:35 +0300

    Add files via upload

---
+++ StringRecursion/StringRecursion/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringRecursion
+{
+    class Program
+    {
+        public static string StringFunc(string str,int index=0,string strPunctuations="",string strOthrer="")
+        {
+            
+            if(index==str.Length)
+            {
+                return strPunctuations + strOthrer;
+            }
+            if(char.IsPunctuation(str[index]))
+            {
+                strPunctuations += str[index];
+            }
+            else
+            {
+                strOthrer += str[index];
+            }
+            return StringFunc(str, index + 1,strPunctuations,strOthrer);
+            
+        }
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the line : ");
+            string str = Console.ReadLine();
+
+            Console.WriteLine("Result = {0}",StringFunc(str));
+        }
+    }
+}
