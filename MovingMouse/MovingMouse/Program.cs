a9d6adb2c0696380ea4f7b2530d51facb6dadba0
SorokaVlad17 <44582306+SorokaVlad17@users.noreply.github.com>
2019-09-14 11:39:49 +0300

    Add files via upload

---
+++ MovingMouse/MovingMouse/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace MovingMouse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //MOVING MOUSE IN CONSOLE!
+
+            int xConsole,yConsole;
+
+            const int SPEED_MOUSE_X = 5;
+
+            const int SPEED_MOUSE_Y = 25;
+
+            int xMonitorBefore = Cursor.Position.X;
+            int yMonitorBefore = Cursor.Position.Y;
+
+            for (; ; )
+            {
+
+                int xMonitorNow = Cursor.Position.X;
+                int yMonitorNow = Cursor.Position.Y;
+
+                xConsole = ((xMonitorNow - yMonitorBefore) / SPEED_MOUSE_X) + Console.CursorLeft ;
+                yConsole =(yMonitorNow + yMonitorBefore) / SPEED_MOUSE_Y + Console.CursorTop -2 ;
+
+                try
+                {
+                   Console.SetCursorPosition(xConsole, yConsole);
+                }
+                catch
+                {
+                    continue;
+                }
+
+                xMonitorBefore = xMonitorNow;
+                yMonitorBefore = yMonitorNow;
+
+                Thread.Sleep(10);
+
+                Console.Clear();
+                
+            }
+        }
+    }
+}
