c6cd83411b44c5decdd2af0e0f5dc7d37236ef64
SorokaVlad17 <44582306+SorokaVlad17@users.noreply.github.com>
2019-10-04 21:53:28 +0300

    Add files via upload

---
+++ WeekScheduleEnum/WeekScheduleEnum/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeekScheduleEnum
+{
+    
+    class Program
+    {
+        //public static Dictionary<string, string> daysOfWeekRus = new Dictionary<string, string>()
+        //{
+        //    {"Monday", "Понедельник" },
+        //    {"Tuesday", "Понедельник" },
+        //    {"Wednesday", "Понедельник" },
+        //    {"Thursday", "Понедельник" },
+        //    {"Friday", "Понедельник" },
+        //    {"Sunday", "Понедельник" },
+        //    {"Saturday", "Понедельник" },
+
+        //};
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
