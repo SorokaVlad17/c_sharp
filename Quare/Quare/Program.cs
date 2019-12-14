afe04a8124542e39e3b065154db53939e6fa5d97
SorokaVlad17 <44582306+SorokaVlad17@users.noreply.github.com>
2019-12-14 08:04:32 +0200

    Add files via upload

---
+++ Quare/Quare/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Quare quare = new Quare(5);
+            quare.Euquare(5);
+            Console.WriteLine(quare.Dequare());
+            Console.WriteLine(quare.Dequare());
+
+        }
+    }
+}
---
+++ Quare/Quare/Quare.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Quare
+{
+    class Quare
+    {
+        #region Private fields
+
+        private object[] _quare;
+        private int _sizeQuare;
+        private int _positionWrite;
+        private int _positionRead;
+
+        #endregion
+        public Quare(int count)
+        {
+            _sizeQuare = count;
+            _positionWrite = 0;
+            _positionRead = 0;
+            _quare = new object[count];
+        }
+
+        #region Methods
+        
+        public bool IsFull()
+        {
+            bool isFull = false;
+            if (_positionWrite == _sizeQuare)
+            {
+                isFull = true;
+            }
+
+            return isFull;
+        }
+
+        public bool IsEmpty()
+        {
+            bool isEmpty = false;
+            if(_quare.Length == 0)
+            {
+                isEmpty = true;
+            }
+
+            return isEmpty;
+        }
+
+        public void Euquare(object item)
+        {
+            if(IsFull())
+            {
+                return;
+            }
+
+            _quare[_positionWrite] = item;
+            ++_positionWrite;
+        }
+
+        public object Dequare()
+        {
+            if(IsEmpty())
+            {
+                return null;
+            }
+            ++_positionRead;
+          
+            return _quare[_positionRead - 1];
+        }
+
+
+    }
+    #endregion
+}
